9539c8117f78dd84aad747f489b876dc45ebf2d5
root <root@localhost.localdomain>
2026-09-12 20:02:00 +0530

    Fix: Disable R8 minification - wrong ProGuard rules were crashing the app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,12 +57,8 @@ android {
             } else {
                 signingConfig = signingConfigs.getByName("debug")
             }
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 
